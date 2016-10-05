ec7438e3f0de1e5ffd9375af0176fcd7463d15e5
Vasiliy K <klimashin.v@gmail.com>
2016-10-05 16:49:13 +0300

    Add tabs

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ angular.module('PinterestService', []).service('pinterest', function() {
 
 });
 
-var app = angular.module('app', ['InstagramService', 'PinterestService']);
-app.controller('AppCtrl', function($scope, instagram, pinterest) {
+var AppCtrl = function($scope, instagram, pinterest) {
+	this.activeTab = 1;
+	this.$scope = $scope;
 
-});
+	this.$scope.openTab = function(tabNumber) {
+		this.activeTab = tabNumber;
+	}.bind(this);
+};
+
+
+var app = angular.module('app', ['InstagramService', 'PinterestService']);
+app.controller('AppCtrl', AppCtrl);
